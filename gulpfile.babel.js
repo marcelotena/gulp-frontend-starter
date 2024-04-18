a946e1f2bdd955f5f133cf773e9a59b608b583be
Marcelo Tena <marcelotena@fastandyours.com>
2024-04-18 09:51:50 +0200

    Rolled back gulp version to 4.0.2 to fix copying issues. Updated Node.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -88,7 +88,7 @@ gulp.task('js', () => {
 // --------------------------------------------------------------------
 
 gulp.task('imgmin', () => {
-    return gulp.src('./src/img/*')
+    return gulp.src('./src/img/*', {debug: true})
         .pipe(plumber())
         .pipe(imagemin([
             gifsicle({interlaced: true}),
